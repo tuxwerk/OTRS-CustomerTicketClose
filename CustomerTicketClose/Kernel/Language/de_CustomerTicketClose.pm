16a7209a390e1c6c50a0c660d5fddb39aca540d9
Marek Kralewski <mck@tuxwerk.de>
2012-03-07 16:33:48 +0100

    Bugfix language file

--- CustomerTicketClose/Kernel/Language/de_CustomerTicketClose.pm
+++ CustomerTicketClose/Kernel/Language/de_CustomerTicketClose.pm
@@ -3,7 +3,7 @@ package Kernel::Language::de_CustomerTicketClose;
 use strict;
 
 sub Data {
-  my $Self-> shift;
+  my $Self = shift;
 
   $Self->{Translation}->{'Close ticket'} = 'Ticket schließen';
   $Self->{Translation}->{'Closing ticket'} = 'Schließen von Ticket';
@@ -15,7 +15,8 @@ sub Data {
   $Self->{Translation}->{'This ticket is already closed.'} = 'Dieses Ticket wurde bereits geschlossen.';
   $Self->{Translation}->{'Thank you for closing the ticket. Thus, the support case is closed for us and will not be pursued!'} = 'Vielen Dank für das Schließen des Tickets. Damit ist der Supportfall für uns abgeschlossen und wird nicht weiterverfolgt!';
   $Self->{Translation}->{'However, should any further questions occur, please contact us again.'} = 'Sollten dennoch weitere Fragen auftreten, bitten wir Sie uns erneut zu kontaktieren.';
-  $Self->{Translation}->{'You can reach us anytime via our <strong><a href="%s">support form</a></strong>.'} = 'Sie können uns jederzeit über unser <strong><a href="%s">Supportformular</a></strong> erreichen.';
+  $Self->{Translation}->{'You can reach us anytime via our support form:'} = 'Sie können uns jederzeit über unser Supportformular erreichen:';
+  $Self->{Translation}->{'Click here'} = 'Hier klicken';
   $Self->{Translation}->{'The given data are not consistent with any ticket. Please check that you have correctly inserted the link into your browser.'} = 'Die angegebenen Daten stimmen mit keinem Ticket überein. Bitte überprüfen Sie, ob Sie den Link richtig in Ihren Browser eingefügt haben.';
   $Self->{Translation}->{'Ticket closed by customer.'} = 'Ticket vom Kunden geschlossen.';
 }
