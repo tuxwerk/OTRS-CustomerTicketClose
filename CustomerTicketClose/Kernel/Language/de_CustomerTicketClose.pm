acdda25873257336ec64a260ff340fe599db79fe
Marek Kralewski <mck@tuxwerk.de>
2013-04-06 18:35:18 +0200

    Bump framework version to 3.1 / change encoding to utf-8

--- CustomerTicketClose/Kernel/Language/de_CustomerTicketClose.pm
+++ CustomerTicketClose/Kernel/Language/de_CustomerTicketClose.pm
@@ -14,19 +14,19 @@ use strict;
 sub Data {
   my $Self = shift;
 
-  $Self->{Translation}->{'Close ticket'} = 'Ticket schließen';
-  $Self->{Translation}->{'Closing ticket'} = 'Schließen von Ticket';
-  $Self->{Translation}->{'What is the reason for closing the ticket?'} = 'Weshalb schließen Sie das Ticket?'; 
+  $Self->{Translation}->{'Close ticket'} = 'Ticket schlieÃŸen';
+  $Self->{Translation}->{'Closing ticket'} = 'SchlieÃŸen von Ticket';
+  $Self->{Translation}->{'What is the reason for closing the ticket?'} = 'Weshalb schlieÃŸen Sie das Ticket?'; 
   $Self->{Translation}->{'Were you content with our service?'} = 'Waren Sie mit unserem Service zufrieden?';
-  $Self->{Translation}->{'You can add a short comment if you like:'} = 'Wenn Sie möchten, schreiben Sie uns einen kurzen Kommentar:';
+  $Self->{Translation}->{'You can add a short comment if you like:'} = 'Wenn Sie mÃ¶chten, schreiben Sie uns einen kurzen Kommentar:';
   $Self->{Translation}->{'Thank you!'} = 'Vielen Dank!';
   $Self->{Translation}->{'Enter comment'} = 'Kommentar eintragen';
   $Self->{Translation}->{'This ticket is already closed.'} = 'Dieses Ticket wurde bereits geschlossen.';
-  $Self->{Translation}->{'Thank you for closing the ticket. Thus, the support case is closed for us and will not be pursued!'} = 'Vielen Dank für das Schließen des Tickets. Damit ist der Supportfall für uns abgeschlossen und wird nicht weiterverfolgt!';
+  $Self->{Translation}->{'Thank you for closing the ticket. Thus, the support case is closed for us and will not be pursued!'} = 'Vielen Dank fÃ¼r das SchlieÃŸen des Tickets. Damit ist der Supportfall fÃ¼r uns abgeschlossen und wird nicht weiterverfolgt!';
   $Self->{Translation}->{'However, should any further questions occur, please contact us again.'} = 'Sollten dennoch weitere Fragen auftreten, bitten wir Sie uns erneut zu kontaktieren.';
-  $Self->{Translation}->{'You can reach us anytime via our support form:'} = 'Sie können uns jederzeit über unser Supportformular erreichen:';
+  $Self->{Translation}->{'You can reach us anytime via our support form:'} = 'Sie kÃ¶nnen uns jederzeit Ã¼ber unser Supportformular erreichen:';
   $Self->{Translation}->{'Click here'} = 'Hier klicken';
-  $Self->{Translation}->{'The given data are not consistent with any ticket. Please check that you have correctly inserted the link into your browser.'} = 'Die angegebenen Daten stimmen mit keinem Ticket überein. Bitte überprüfen Sie, ob Sie den Link richtig in Ihren Browser eingefügt haben.';
+  $Self->{Translation}->{'The given data are not consistent with any ticket. Please check that you have correctly inserted the link into your browser.'} = 'Die angegebenen Daten stimmen mit keinem Ticket Ã¼berein. Bitte Ã¼berprÃ¼fen Sie, ob Sie den Link richtig in Ihren Browser eingefÃ¼gt haben.';
   $Self->{Translation}->{'Ticket closed by client.'} = 'Ticket vom Kunden geschlossen.';
   $Self->{Translation}->{'Closed by client'} = 'Vom Kunden geschlossen';
   $Self->{Translation}->{'Comment'} = 'Kommentar';
